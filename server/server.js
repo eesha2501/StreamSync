d1a12b7e97f5fd9c832449203370cdd113e663d2
Eesha Singh <121451707+eesha2501@users.noreply.github.com>
2025-07-22 00:50:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors({ origin: 'https://streamsync-web.vercel.app/' }));
+app.use(cors({ origin: 'https://streamsync-web.vercel.app' }));
 app.use(express.json());
 
 // --- NEW DEBUGGING MIDDLEWARE ---
